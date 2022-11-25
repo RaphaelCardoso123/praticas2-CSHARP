93c2f03182afd89e83040ddfcf4f9464eae3495a
RaphaelCardoso123 <91799151+RaphaelCardoso123@users.noreply.github.com>
2022-11-25 16:58:29 -0300

    ..

---
+++ Praticas2/Praticas2/Enums/Client.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Praticas2.Enums
+{
+    class Client
+    {
+        public string  Name { get; set; }
+        public string Email { get; set; }
+        public DateTime birthDate { get; set; } //birthDate = DateTime?
+    }
+}
