cb31e24f147b3e61d9d751cc71090f8b38aa66d3
Raphael Cardoso <cardoso.raphael@hotmail.com>
2022-12-08 10:17:05 -0300

    ...

---
+++ Praticas2/Praticas2_A/Entities/BussinesAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Praticas2_A.Entities
+{
+    class BussinesAccount : Account
+    {
+
+        public double LoanLimit { get; set; }
+
+        public BussinesAccount()
+        {
+        }
+
+        public BussinesAccount(int number, string holder, double balance, double loanLimit) : base(number, holder, balance)
+        {
+            //para não ter que refazer aqui tudo da Account a gente usa o :base(number, holder, balance)
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if(amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
---
+++ Praticas2/Praticas2_A/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Praticas2_A.Entities;
+namespace Praticas2_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BussinesAccount account = new BussinesAccount(8000, "Raphael", 3500.00, 10000.00);
+            Console.WriteLine(account.Balance);
+
+            Console.ReadLine();
+
+        }
+    }
+}
