ace73748f1109b87eded6be1340742c8f847bd12
Raphael Cardoso <cardoso.raphael@hotmail.com>
2022-12-08 11:57:36 -0300

    ...

---
+++ Praticas2/Praticas2_A/Entities/SavingAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Praticas2_A.Entities
+{
+    class SavingAccount : Account
+    {
+
+        public double InterestRate { get; set; }
+
+        public SavingAccount()
+        {
+        }
+
+        public SavingAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            //para não ter que refazer aqui tudo da Account a gente usa o :base(number, holder, balance)
+            InterestRate = InterestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+    }
+}
--- Praticas2/Praticas2_A/Program.cs
+++ Praticas2/Praticas2_A/Program.cs
@@ -6,12 +6,43 @@ namespace Praticas2_A
     {
         static void Main(string[] args)
         {
+            // HERANÇA 
+            //BussinesAccount account = new BussinesAccount(8000, "Raphael", 3500.00, 10000.00);
+            //Console.WriteLine(account.Balance);
 
-            BussinesAccount account = new BussinesAccount(8000, "Raphael", 3500.00, 10000.00);
-            Console.WriteLine(account.Balance);
+
+            // UPCASTING
+            Account acc = new Account(8000, "Raphael", 0.0);
+            BussinesAccount bacc = new BussinesAccount(7000, "Flávia", 0.0, 4500.00);
+
+            Account acc1 = bacc;
+            Account acc2 = new BussinesAccount(6000, "Júlia", 0.0, 3000.00);
+            Account acc3 = new SavingAccount(500, "Kelly", 0.0, 0.01);
+
+
+            // DOWNCASTING
+            BussinesAccount acc4 = (BussinesAccount)acc2;
+            acc4.Loan(100.00);
+
+
+            // IS
+            if(acc3 is BussinesAccount)
+            {
+                BussinesAccount acc5 = (BussinesAccount)acc3;
+                acc5.Loan(200.00);
+                Console.WriteLine("Loan!!");
+            }
+            if(acc3 is SavingAccount)
+            {
+                SavingAccount acc5 = (SavingAccount)acc3;
+                acc5.UpdateBalance();
+                Console.WriteLine("Update!!");
+            }
+
+
+
 
             Console.ReadLine();
-
         }
     }
 }
