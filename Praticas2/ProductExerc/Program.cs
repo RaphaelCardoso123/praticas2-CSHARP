92f4a9cf916011e755da616c1b598a83215e8f21
Raphael Cardoso <cardoso.raphael@hotmail.com>
2022-12-09 10:37:01 -0300

    ...

---
+++ Praticas2/ProductExerc/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductExerc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
---
+++ Praticas2/ProductExerc/UsedProduct.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductExerc
+{
+    class UsedProduct : Product
+    {
+
+        public DateTime ManufactureDate { get; set; }
+
+        public UsedProduct()
+        {
+        }
+
+        public UsedProduct(DateTime manufactureDate)
+        {
+            ManufactureDate = manufactureDate;
+        }
+
+        public override string PriceTag()
+        {
+            return base.PriceTag();
+        }
+
+    }
+}
