8e1df6ef33dca3aaa1d32a5b859c00678a759f8a
Raphael Cardoso <cardoso.raphael@hotmail.com>
2022-12-12 14:26:43 -0300

    ...

--- Praticas2/Praticas2_A/Program.cs
+++ Praticas2/Praticas2_A/Program.cs
@@ -10,31 +10,31 @@ namespace Praticas2_A
         static void Main(string[] args)
         {
 
-        // HERANÇA / UPCASTING / DOWNCASTING / IS / AS / SOBREPOSIÇÃO(SOBREESCRITA) VIRTUAL - OVERRIDE / BASE / CLASSE ABSTRATA
+            // HERANÇA / UPCASTING / DOWNCASTING / IS / AS / SOBREPOSIÇÃO(SOBREESCRITA) VIRTUAL - OVERRIDE / BASE / CLASSE ABSTRATA(a escrita é em itálico na descrição)
 
-        List<Account> list = new List<Account>();
+            List<Account> list = new List<Account>();
 
-        list.Add(new SavingAccount(1001, "Alex", 500.00, 0.01));
-        list.Add(new BussinesAccount(1002, "Maria", 500.00, 400.00));
-        list.Add(new SavingAccount(1003, "Bob", 500.00, 0.01));
-        list.Add(new BussinesAccount(1004, "Anna", 500.00, 500.00));
+            list.Add(new SavingAccount(1001, "Alex", 500.00, 0.01));
+            list.Add(new BussinesAccount(1002, "Maria", 500.00, 400.00));
+            list.Add(new SavingAccount(1003, "Bob", 500.00, 0.01));
+            list.Add(new BussinesAccount(1004, "Anna", 500.00, 500.00));
 
 
-        double sum = 0.0;
-        foreach (Account acc in list)
+            double sum = 0.0;
+            foreach (Account acc in list)
             {
                 sum += acc.Balance;
             }
-        Console.WriteLine("Total balance: " + sum.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Total balance: " + sum.ToString("F2", CultureInfo.InvariantCulture));
 
 
-        foreach (Account acc in list)
+            foreach (Account acc in list)
             {
                 acc.Withdraw(10.00);
             }
-        foreach (Account acc in list)
+            foreach (Account acc in list)
             {
-                Console.WriteLine("Updated balance for account " + acc.Number + ") " 
+                Console.WriteLine("Updated balance for account " + acc.Number + ") "
                 + acc.Balance.ToString("F2", CultureInfo.InvariantCulture));
             }
 
