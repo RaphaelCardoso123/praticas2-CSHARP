02ce943f12982a67caed48eeaab8329caa25c05b
Raphael Cardoso <cardoso.raphael@hotmail.com>
2023-02-03 15:56:03 -0300

    ...

--- Praticas2/Praticas2_D/Program.cs
+++ Praticas2/Praticas2_D/Program.cs
@@ -10,10 +10,13 @@ namespace Praticas2_D
         {
 
             Console.WriteLine("Enter rental date");
+
             Console.Write("Car model: ");
             string model = Console.ReadLine();
+
             Console.Write("Pickup (dd/MM/yyyy hh:mm): ");
             DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy hh:mm", CultureInfo.InvariantCulture);
+
             Console.Write("Return (dd/MM/yyyy hh:mm): ");
             DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy hh:mm", CultureInfo.InvariantCulture);
 
