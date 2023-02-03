80ee63feff7bb02e2fda2537d09403613baed4d5
Raphael Cardoso <cardoso.raphael@hotmail.com>
2023-02-03 11:52:23 -0300

    working_in_progress

---
+++ Praticas2/Praticas2_D/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Praticas2_D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
