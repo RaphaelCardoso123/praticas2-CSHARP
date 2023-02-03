ae0ea5545e77d7c483d58351ad3e09bf6cd753dd
Raphael Cardoso <cardoso.raphael@hotmail.com>
2023-02-03 14:16:57 -0300

    ...

--- Praticas2/Praticas2_D/Program.cs
+++ Praticas2/Praticas2_D/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Globalization;
 
 namespace Praticas2_D
 {
@@ -8,6 +7,16 @@ namespace Praticas2_D
     {
         static void Main(string[] args)
         {
+
+            Console.Write("Enter rental date");
+            Console.Write("Car model: ");
+            string model = Console.ReadLine();
+            Console.Write("Pickup (dd/MM/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy hh:mm", CultureInfo.InvariantCulture);
+            Console.Write("Return (dd/MM/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy hh:mm", CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
         }
     }
 }
