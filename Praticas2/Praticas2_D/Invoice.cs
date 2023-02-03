dae7b7a0fafda49a19424f9baa4e5376d802cf9e
Raphael Cardoso <cardoso.raphael@hotmail.com>
2023-02-03 13:43:41 -0300

    Novo Projeto

--- Praticas2/Praticas2_D/Invoice.cs
+++ Praticas2/Praticas2_D/Invoice.cs
@@ -1,10 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Globalization;
 
 namespace Praticas2_D
 {
     class Invoice
     {
+
+        public double BasicPayment { get; set; }
+        public double Tax { get; set; }
+
+        public Invoice(double basicPayment, double tax)
+        {
+            BasicPayment = basicPayment;
+            Tax = tax;
+        }
+
+        public double TotalPayment
+        {
+            get { return BasicPayment + Tax; }
+        }
+
+        public override string ToString()
+        {
+            return "Basic Payment: "
+                + BasicPayment.ToString("F2", CultureInfo.InvariantCulture)
+                + Tax.ToString("F2", CultureInfo.InvariantCulture)
+                + TotalPayment.ToString("F2", CultureInfo.InvariantCulture);
+        }
     }
 }
